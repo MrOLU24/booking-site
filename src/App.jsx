24bad0cc50a290df95b6fcc61dabecc9d73d2fbd
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-04-10 23:22:52 +0100

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import TodoList from './Components/TodoList'
+import Profile from './Components/Profile'
+
 
 const App = () => {
   return (
     <div>
-      <TodoList/>
+    <Profile/>
     </div>
   )
 }
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Profile = () => {
+const UserProfile = useState({
+    name: olatunji,
+    Age: 25,
+})
+
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
